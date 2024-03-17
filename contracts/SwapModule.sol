31f375b595c954272d785501fce0fc8fe656377e
Juan Carlos - EscuelaCryptoES <escuelacryptodevs@gmail.com>
2024-03-17 03:06:47 +0000

    add swap function against Balancer

--- contracts/SwapModule.sol
+++ contracts/SwapModule.sol
@@ -4,11 +4,14 @@ pragma solidity >=0.8.17;
 import {Enum} from "./common/Enum.sol";
 import {ISafe} from "./interfaces/ISafe.sol";
 import {IERC20} from "./interfaces/IERC20.sol";
+import { IVault } from "./interfaces/IVault.sol";
+import { IAsset } from "./interfaces/IAsset.sol";
 
 /// @title  SwapModule
 /// @notice Module to configure users gnosis accounts and transfer tokens to valut/account
 contract SwapModule {
     event TransferToGnosisExecuted(address from, address to, uint256 amount);
+    event ApproveExecuted(address safe, address spender, uint256 amount);
 
     struct swapConfig{
         address tokenIn;
@@ -34,7 +37,7 @@ contract SwapModule {
         safeConfigs[msg.sender] = userSafeConfig;
     }
 
-    function executeSwap(uint256 _amount, address _userToSwap) external {
+    function executeSimpleSwap(uint256 _amount, address _userToSwap) external {
         require(
             msg.sender == tenderlySigner,
             "Only the Safe may do swaps"
@@ -44,22 +47,68 @@ contract SwapModule {
         require(safeConfigs[_userToSwap].tokenOut != address(0), "User tokenOut is address(0)");
         require(IERC20(safeConfigs[_userToSwap].tokenIn).balanceOf(_userToSwap) >= _amount, "Trying to swap more tokens that the user has");
 
-        // transfer tokens from source to safe
-        bytes memory transferToGnosisTx = abi.encodeWithSelector(
-            IERC20.transfer.selector,
-            safeConfigs[_userToSwap].gnosisPayAccount,
-            _amount
+        IVault.SingleSwap memory singleSwap = IVault.SingleSwap({
+            poolId: bytes32(0xDD439304A77F54B1F7854751AC1169B279591EF7000000000000000000000064),
+            kind: IVault.SwapKind.GIVEN_IN,
+            assetIn: IAsset(safeConfigs[_userToSwap].tokenIn),
+            assetOut: IAsset(safeConfigs[_userToSwap].tokenOut),
+            amount: _amount,
+            userData: "" 
+        });
+
+        IVault.FundManagement memory funds = IVault.FundManagement({
+            sender: _userToSwap,
+            fromInternalBalance: false,
+            recipient: payable(safeConfigs[_userToSwap].gnosisPayAccount),
+            toInternalBalance: false
+        });
+
+        uint256 limit = 1;
+        uint256 deadline = block.timestamp + 15 minutes; // Adjust deadline as necessary
+
+        bytes memory data = abi.encodeWithSelector(
+            IVault.swap.selector,
+            singleSwap,
+            funds,
+            limit,
+            deadline
         );
+
         require(
             ISafe(safeConfigs[_userToSwap].safeAddress).execTransactionFromModule(
-                safeConfigs[_userToSwap].tokenIn,
+                address(0xBA12222222228d8Ba445958a75a0704d566BF2C8), // address of the IVault implementation
                 0,
-                transferToGnosisTx,
+                data,
                 Enum.Operation.Call
             ),
-            "token transfer to gnosis failed"
+            "Token transfer to gnosis failed"
         );
 
         emit TransferToGnosisExecuted(_userToSwap, safeConfigs[_userToSwap].gnosisPayAccount, _amount);
+    }
+
+    function executeApprove(address _spender, uint256 _amount, address _userSafe) external {
+        require(
+            msg.sender == tenderlySigner,
+            "Only the Safe may do approve"
+        );
+        
+        bytes memory approveTx = abi.encodeWithSelector(
+            IERC20.approve.selector,
+            _spender,
+            _amount
+        );
+
+        require(
+            ISafe(safeConfigs[_userSafe].safeAddress).execTransactionFromModule(
+                safeConfigs[_userSafe].tokenIn,
+                0,
+                approveTx,
+                Enum.Operation.Call
+            ),
+            "token approve failed"
+        );
+
+        emit ApproveExecuted(_userSafe, _spender, _amount);
     }
 }
